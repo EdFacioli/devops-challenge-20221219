f00deedcb5aaa6526edd88c0f2bed657663a7f37
Edson Facioli <edfacioli@gmail.com>
2022-12-28 09:27:53 -0300

    Configure backend remote in s3 bucket

---
+++ setup.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket = "devops-test-benice"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-2"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
